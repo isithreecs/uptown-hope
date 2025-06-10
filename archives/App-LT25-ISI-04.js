cf9cec9dc85a243a77e182642b07e37bb340e84d
Chris_Connor <christopher.connorjr1@gmail.com>
2025-06-10 14:32:16 -0400

    File structure layout
    Restructured the layout of the explorer to get rid of duplicate pages.

--- archives/App-LT25-ISI-04.js
+++ archives/App-LT25-ISI-04.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Route, Routes } from 'react-router-dom'
-import MainNav from './components/MainNav/MainNav';
-import Footer from './components/Footer/Footer';
+import MainNav from '../src/components/MainNav/MainNav';
+import Footer from '../src/components/Footer/Footer';
 import Chatbot from './components/Chatbot/Chatbot';
 
 import Home from './pages/HomePage/Home';
 import About from './pages/AboutPage/About';
 import Services from './pages/ServicesPage/Services';
 import CareerOpportunities from './pages/CareerOpportunities/CareerOpportunities';
-import Contact from './pages/ContactPage/Contact';
+import Contact from '../src/pages/ContactPage/Contact';
 import StaffingSolutions from './pages/StaffingSolutionsPage/StaffingSolutions';
 
 // new pages
@@ -17,10 +17,10 @@ import Employees from './pages/EmployeePage/Employees';
 import Login from './pages/Login/Login';
 import SignUp from './pages/Login/SignUp';
 
-import CareerOpportunities2 from './pages/CareerOpportunities/CareerOpportunities2';
-import About2 from './pages/AboutPage/About2';
-import StaffingSolutions2 from './pages/StaffingSolutionsPage/StaffingSolutions2';
-import Home2 from './pages/HomePage/Home2';
+import CareerOpportunities2 from '../src/pages/CareerOpportunities/CareerOpportunities2';
+import About2 from '../src/pages/AboutPage/About2';
+import StaffingSolutions2 from '../src/pages/StaffingSolutionsPage/StaffingSolutions2';
+import Home2 from '../src/pages/HomePage/Home2';
 import Services2 from './pages/ServicesPage/Services2';
 
 class App extends Component {
--- src/App.js
+++ src/App.js
@@ -3,22 +3,22 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom'
 import MainNav from './components/MainNav/MainNav';
 import Footer from './components/Footer/Footer';
-import About from './pages/AboutPage/About';
-import Services from './pages/ServicesPage/Services';
-import CareerOpportunities from './pages/CareerOpportunities/CareerOpportunities';
-import Contact from './pages/ContactPage/Contact';
-import StaffingSolutions from './pages/StaffingSolutionsPage/StaffingSolutions';
+// import About from './pages/AboutPage/About';
+// import Services from './pages/ServicesPage/Services';
+// import CareerOpportunities from './pages/CareerOpportunities/CareerOpportunities';
+// import StaffingSolutions from './pages/StaffingSolutionsPage/StaffingSolutions';
 
 // new pages
 // import Login from './pages/Login/Login';
 // import SignUp from './pages/Login/SignUp';
 
-import Employees from './pages/EmployeePage/Employees';
+// import Employees from './pages/EmployeePage/Employees';
+import Contact from './pages/ContactPage/Contact';
 import CareerOpportunities2 from './pages/CareerOpportunities/CareerOpportunities2';
 import About2 from './pages/AboutPage/About2';
 import StaffingSolutions2 from './pages/StaffingSolutionsPage/StaffingSolutions2';
 import Home2 from './pages/HomePage/Home2';
-import Services2 from './pages/ServicesPage/Services2';
+// import Services2 from './pages/ServicesPage/Services2';
 
 class App extends Component {
   
@@ -30,18 +30,18 @@ class App extends Component {
         <div>
           <Routes>
             <Route path='/' element={<Home2 />} />
-            <Route path='/about' element={<About />} />
+            {/* <Route path='/about' element={<About />} />
             <Route path='/services' element={<Services />} />
             <Route path='/employment/' element={<CareerOpportunities />} />
             <Route path='/staffing-solutions' element={<StaffingSolutions />} />
+            
+            <Route path='/employees' element={<Employees />} /> */}
             <Route path='/contact' element={<Contact />} />
-
-            <Route path='/employees' element={<Employees />} />
             <Route path='/career-opportunities' element={<CareerOpportunities2 />} />
             <Route path='/about2' element={<About2 />} />
             <Route path='/StaffingSolutions2' element={<StaffingSolutions2 />} />
             <Route path='/Home2' element={<Home2 />} />
-            <Route path='/Services2' element={<Services2 />} />
+            {/* <Route path='/Services2' element={<Services2 />} /> */}
           </Routes>
         </div>
         <Footer />
